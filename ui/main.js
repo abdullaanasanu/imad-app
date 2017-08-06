9563c79578dac7b70cb5ae75a928c929490c8607
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-06 20:34:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,11 @@ element.innerHTML = "Hello Friends :)";
 //moving image
 
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '300px';
+    var interval = setinterval(moveRight, 100);
 };
