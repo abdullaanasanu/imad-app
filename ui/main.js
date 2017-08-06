c38b2c0fbd9d8bb738bc0a12ddeb24919833cb2c
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-06 21:37:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,14 @@ function moveRight() {
 img.onclick = function() {
     var interval = setInterval(moveRight, 30);
 };
+
+var counter = 0;
+
+var like_button = document.getElementById("likes");
+like_button.onclick = function(){
+    
+    
+    counter = counter + 1;
+    var span = document.getElementById("result");
+    span.innerHTML = counter.toString();
+};
