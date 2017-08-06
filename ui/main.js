6db99ebc4101d3e859c7078dc3ca341dc8637ceb
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-06 19:57:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,4 @@ console.log('Loaded!');
 //changing element of id main div
 
 var element = document.getElementById("main");
-element.innerHTML = "Hello Friends :)"
+element.innerHTML = "Hello Friends :)";
