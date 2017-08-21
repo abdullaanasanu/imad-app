ce5f697195e838698b5a6a7cbfc44b9958ff3fb5
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-21 20:36:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function createTemplate(data){
 
 function hash(input, salt) {
     
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 12, 'sha512');
     return hashed.toString('hex');
     
 }
