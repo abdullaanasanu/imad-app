2ac618be2ff439809ec7e031384ac85b3d939518
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-21 21:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var Config = {
     user : 'abdullaanasanu',
@@ -14,6 +15,7 @@ var Config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var counter = 0;
 app.get('/counter', function (req, res) {
@@ -73,6 +75,22 @@ function hash(input, salt) {
 app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'random-wow-haha');
     res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    pool.query('INSERT INTO "users" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        }else{
+            res.send('User successfully created: '+username);
+        } 
+    });
+    
 });
 
 var pool = new Pool(Config);
