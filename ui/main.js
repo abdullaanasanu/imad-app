8ab208b3428ae43dfa7fc448770c045a705c8030
abdullaanasanu <abdullaanasanu@gmail.com>
2017-08-23 20:40:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,3 +63,35 @@ submit.onclick = function() {
     
 
 };
+
+var submit = document.getElementById('register');
+submit.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+        if (request.readyState === XMLHttpRequest.DONE) {
+            
+            if (request.status === 200) {
+                console.log("Registered");
+                alert('Registered Successfully!');
+            }else if (request.status === 403) {
+                alert('Username Already in Use!');
+            }else if (request.status === 500) {
+                alert('Something went wrong!');
+            }
+            
+        }
+        
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://abdullaansanau.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username : usernmae, password : password}));
+    
+};
